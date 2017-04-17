07f7fc3f6c9d8d2e76be92b3ba3bf90ea3510c21
Raja Simon <rajasimon@icloud.com>
2017-04-17 10:51:12 +0530

    Command line interface initial version to take file args

---
+++ sqlg/cli.py
@@ -0,0 +1,55 @@
+"""
+Command Line Interface
+"""
+import sys
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CommandLineInterface(object):
+    
+    description = "Standardized Query Language Generator"
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description=self.description)
+        self.parser.add_argument(
+            '-i',
+            dest='filename',
+            help='Please give a model file name',
+            default=None
+        )
+        self.parser.add_argument(
+            '-v',
+            '--verbosity',
+            type=int,
+            help='How verbose to make the output',
+            default=1,
+        )
+
+    @classmethod
+    def entrypoint(cls):
+        """
+        Main entrypoint for external starts.
+        """
+        cls().run(sys.argv[1:])
+
+    def run(self, args):
+        """
+        Pass in raw argument list and it will decode them
+        and run the server
+        """
+        # Decode args
+        args = self.parser.parse_args(args)
+        # Set up logging
+        logging.basicConfig(
+            level={
+                0: logging.WARN,
+                1: logging.INFO,
+                2: logging.DEBUG,
+            }[args.verbosity],
+            format="%(asctime)-15s %(levelname)-8s %(message)s",
+        )
+
+        logger.info('Starting sql...')
